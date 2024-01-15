6ef877dc4f918f13c2e34155c7808cfde0544692
janinocencio <janinocencio@gmail.com>
2024-01-16 09:57:21 +1100

    Create function createDivBook and fix formatting

--- js/script.js
+++ js/script.js
@@ -1,30 +1,76 @@
 const myLibrary = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, alreadyRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.alreadyRead = alreadyRead;
 };
 
-function addBookToLibrary() {
-    myLibrary[myLibrary.length] = new Book("test", "test", 5); //enter book fields
+function addBookToLibrary(title, author, pages, alreadyRead) {
+    myLibrary[myLibrary.length] = new Book(title, author, pages, alreadyRead); 
 };  
 
-const addBookBtn = document.querySelector('#addBook');
-const popupForm = document.querySelector('.popup');
-const disableHTML = document.querySelector('#disable');
-addBookBtn.addEventListener('click', () => {
-    popupForm.style.transition = '0.3s ease';
-    popupForm.style.visibility = 'visible';
-    popupForm.style.scale = '1';
-    disableHTML.style.visibility = 'visible';
-    
-});
-
-disableHTML.addEventListener('click', () => {
+function exitForm() {
     popupForm.style.visibility = 'hidden';
     popupForm.style.transition = '0.3s ease';
     popupForm.style.scale = '0.1';
     disableHTML.style.visibility = 'hidden';
     disableHTML.style.transition = '0.3s ease';
+};
+
+function popForm() {
+    popupForm.style.transition = '0.3s ease';
+    popupForm.style.visibility = 'visible';
+    popupForm.style.scale = '1';
+    disableHTML.style.visibility = 'visible';
+};
+
+function createDivBook() {
+    const parentDiv = document.querySelector('.books-container');
+    const div = document.createElement('div');
+    const pTitle = document.createElement('p');
+    const pAuthor = document.createElement('p')
+    const pPages = document.createElement('p');
+    const readBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
+    div.classList.add('book');
+    readBtn.setAttribute('type', 'button');
+    removeBtn.setAttribute('type', 'button');
+    removeBtn.textContent = "Remove";
+
+    pTitle.textContent = myLibrary[myLibrary.length - 1].title;
+    pAuthor.textContent = myLibrary[myLibrary.length - 1].author;
+    pPages.textContent = myLibrary[myLibrary.length - 1].pages;
+    console.log(myLibrary[myLibrary.length - 1].alreadyRead);
+    if (myLibrary[myLibrary.length - 1].alreadyRead === true) readBtn.textContent = "Done read";
+    if (myLibrary[myLibrary.length - 1].alreadyRead === false) readBtn.textContent = "Not read";
+    
+    div.appendChild(pTitle);
+    div.appendChild(pAuthor);
+    div.appendChild(pPages);
+    div.appendChild(readBtn);
+    div.appendChild(removeBtn);
+    parentDiv.appendChild(div);
+};
+
+const popupForm = document.querySelector('.popup');
+const addBookBtn = document.querySelector('#addBook');
+const disableHTML = document.querySelector('#disable');
+const submitBtn = document.querySelector('#submitBtn');
+
+addBookBtn.addEventListener('click', popForm);
+disableHTML.addEventListener('click', exitForm);
+submitBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    const formBook = document.querySelector('form');
+    const titleInput = document.querySelector('#title').value;
+    const authorInput = document.querySelector('#author').value;
+    const pagesInput = document.querySelector('#pages').value;
+    const readInput = document.querySelector('#read').checked;
+    addBookToLibrary(titleInput, authorInput, pagesInput, readInput);
+    createDivBook();
+    exitForm();
+    formBook.reset();
 });
+
