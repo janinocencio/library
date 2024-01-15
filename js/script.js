a15dfa18281bd9201aeb4dca5340ee283365c3bf
janinocencio <janinocencio@gmail.com>
2024-01-15 13:23:02 +1100

    Add add book form open-close functionality

--- js/script.js
+++ js/script.js
@@ -9,3 +9,22 @@ function Book(title, author, pages) {
 function addBookToLibrary() {
     myLibrary[myLibrary.length] = new Book("test", "test", 5); //enter book fields
 };  
+
+const addBookBtn = document.querySelector('#addBook');
+const popupForm = document.querySelector('.popup');
+const disableHTML = document.querySelector('#disable');
+addBookBtn.addEventListener('click', () => {
+    popupForm.style.transition = '0.3s ease';
+    popupForm.style.visibility = 'visible';
+    popupForm.style.scale = '1';
+    disableHTML.style.visibility = 'visible';
+    
+});
+
+disableHTML.addEventListener('click', () => {
+    popupForm.style.visibility = 'hidden';
+    popupForm.style.transition = '0.3s ease';
+    popupForm.style.scale = '0.1';
+    disableHTML.style.visibility = 'hidden';
+    disableHTML.style.transition = '0.3s ease';
+});
